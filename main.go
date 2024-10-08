b8473d7913f8030089ced6603d39b57d18b77710
kapitz01 <tim.kapitz@ptb.de>
2024-10-08 12:28:43 +0200

    commenting

--- main.go
+++ main.go
@@ -7,22 +7,23 @@ import (
 )
 
 var (
-	running bool
-	mu      sync.Mutex
+	running bool       // Flag to indicate if the game is running
+	mu      sync.Mutex // Mutex to synchronize access to shared variables
 )
 
 func main() {
-	running = true
-	go checkInput()
+	running = true  // Set the game state to running
+	go checkInput() // Start the input checking in a separate goroutine
 
+	// Main Gameloop
 	for running {
 		mu.Lock()
-		fmt.Println("Game is running...")
+		fmt.Println("Game is running...") // Display the game status (debug)
 		mu.Unlock()
 
-		time.Sleep(100 * time.Millisecond)
-		clearScreen() // Bildschirm löschen, um das nächste Frame anzuzeigen
+		time.Sleep(100 * time.Millisecond) // Control the game update rate
+		clearScreen()                      // Clear the Screen to show the next frame
 	}
 
-	fmt.Println("Game ended.")
+	fmt.Println("Game ended.") // Display end message (debug)
 }
