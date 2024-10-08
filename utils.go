d3b101b071920ef3b598b9919877886d772cea8a
kapitz01 <tim.kapitz@ptb.de>
2024-10-08 12:50:29 +0200

    switched rendering method

--- utils.go
+++ utils.go
@@ -12,42 +12,37 @@ func clearScreen() {
 	if runtime.GOOS == "windows" {
 		cmd := exec.Command("cmd", "/c", "cls") // Command to clear the screen on Windows
 		cmd.Stdout = os.Stdout
-		err := cmd.Run()
-		if err != nil {
-			return
-		}
+		cmd.Run()
 	} else {
 		cmd := exec.Command("clear") // Command to clear the screen on Unix-like systems
 		cmd.Stdout = os.Stdout
-		err := cmd.Run()
-		if err != nil {
-			return
-		}
+		cmd.Run()
 	}
 }
 
-// renderFrame renders the framebuffer to the console if it has changed
+// renderFrame renders the entire framebuffer to the console
 func renderFrame() {
-	// Check for changes and render only if needed
+	// Clear the screen first
+	clearScreen()
+
+	// Render the entire framebuffer
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
-			if framebuffer[y][x] != lastFrame[y][x] {
-				os.Stdout.WriteString(fmt.Sprintf("\033[%d;%dH%c", y+1, x+1, framebuffer[y][x]))
-				lastFrame[y][x] = framebuffer[y][x] // Update lastFrame
-			}
+			// Print the character at (x, y)
+			os.Stdout.WriteString(fmt.Sprintf("\033[%d;%dH%c", y+1, x+1, framebuffer[y][x]))
 		}
 	}
 }
 
 // updateFrame updates the framebuffer with the current game state
 func updateFrame() {
-	// Example: Fill the framebuffer with a character for testing
+	// Example: Fill the framebuffer with a character (for testing)
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
 			framebuffer[y][x] = ' ' // Clear the framebuffer
-			// Gamelogic area
-			if x == 5 && y == 5 {
-				framebuffer[y][x] = '#'
+			// game logic
+			if x == 5 && y == 5 { // Example: set a character at (5, 5)
+				framebuffer[y][x] = 'X'
 			}
 		}
 	}
